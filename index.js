f506fcdb78b4193e338d66d246d2a6504cf4b9e8
md4sd <cxerkf@mailfence.com>
2023-04-09 12:47:04 +0000

    Update 13 files

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+// const port = process.env.PORT || 3000;
+const port = 3000;
 const { execSync } = require("child_process");
 
-execSync(`chmod +x ./start.sh && ./start.sh`,{
-	cwd: './'
-})
+execSync(`chmod +x ./start.sh && ./start.sh`, {
+  cwd: "./",
+});
